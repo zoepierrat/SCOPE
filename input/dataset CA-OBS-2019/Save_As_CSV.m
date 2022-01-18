b3dd03f5c9c899b6982d21f43861f63040441cb5
Zoe Pierrat <zpierrat@gmail.com>
2022-01-18 13:59:54 -0800

    Update Input to hPa
    Updated SOBS_input file with correct conversion for pressure variables

--- input/dataset CA-OBS-2019/Save_As_CSV.m
+++ input/dataset CA-OBS-2019/Save_As_CSV.m
@@ -4,7 +4,7 @@ y = year(OBS_AirTemp_Cnpy6m.Timestamp);
 DOY = day(OBS_AirTemp_Cnpy6m.Timestamp,"dayofyear");
 doy = DOY + (minute(OBS_AirTemp_Cnpy6m.Timestamp)/60+hour(OBS_AirTemp_Cnpy6m.Timestamp))/24;
 
-T = table(y,doy,OBS_IncomingShortwaveRad_AbvCnpy.Value,OBS_IncomingLongwaveRad_AbvCnpy.Value,OBS_BarometricPressure.Value,OBS_AirTemp_AbvCnpy25m.Value,OBS_WindSpeed_AbvCnpy26m.Value,OBS_VapourPressure_AbvCnpy25m.Value);
+T = table(y,doy,OBS_IncomingShortwaveRad_AbvCnpy.Value,OBS_IncomingLongwaveRad_AbvCnpy.Value,OBS_BarometricPressure.Value*10,OBS_AirTemp_AbvCnpy25m.Value,OBS_WindSpeed_AbvCnpy26m.Value,OBS_VapourPressure_AbvCnpy25m.Value*10); %added conversions 
 
 T.Properties.VariableNames = {'y','t','Rin','Rli','p','Ta','u','ea'};
 
