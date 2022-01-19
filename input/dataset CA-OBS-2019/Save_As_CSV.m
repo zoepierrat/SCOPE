7e513e1ae026ea5ad484c4c9ae0d5c3c435c0120
Zoe Pierrat <zpierrat@gmail.com>
2022-01-19 07:10:33 -0800

    New input file with times in Local not UTC

--- input/dataset CA-OBS-2019/Save_As_CSV.m
+++ input/dataset CA-OBS-2019/Save_As_CSV.m
@@ -2,7 +2,7 @@ load("SOBS_2018-2021.mat")
 
 y = year(OBS_AirTemp_Cnpy6m.Timestamp);
 DOY = day(OBS_AirTemp_Cnpy6m.Timestamp,"dayofyear");
-doy = DOY + (minute(OBS_AirTemp_Cnpy6m.Timestamp)/60+hour(OBS_AirTemp_Cnpy6m.Timestamp))/24;
+doy = DOY + (minute(OBS_AirTemp_Cnpy6m.Timestamp)/60+hour(OBS_AirTemp_Cnpy6m.Timestamp)-6)/24; %put into UTC 
 
 T = table(y,doy,OBS_IncomingShortwaveRad_AbvCnpy.Value,OBS_IncomingLongwaveRad_AbvCnpy.Value,OBS_BarometricPressure.Value*10,OBS_AirTemp_AbvCnpy25m.Value,OBS_WindSpeed_AbvCnpy26m.Value,OBS_VapourPressure_AbvCnpy25m.Value*10); %added conversions 
 
